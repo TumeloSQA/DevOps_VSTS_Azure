978c103d808aed9ac2dae8b10e46434440cb6df3
Tumelo <tumelophuti@gmail.com>
2017-07-12 15:29:04 +0200

    Web Api and MVC Unit Testing

---
+++ DevOps_VSTS_Azure.Tests/Controllers/EmployeeController.cs
@@ -0,0 +1,84 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Text;
+//using System.Web.Http;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using DevOps_VSTS_Azure;
+
+//using DevOps_VSTS_Azure.Tests.Controllers;
+//using DevOps_VSTS_Azure.Controllers;
+
+//namespace DevOps_VSTS_Azure.Tests.Controllers
+//{
+//  [TestClass]
+//  public class ValuesControllerTest
+//  {
+//      [TestMethod]
+//      public void Get()
+//      {
+//          // Arrange
+//          EmployeeManagementController controller = new EmployeeManagementController();
+
+//          // Act
+//          IEnumerable<string> result = controller.Get();
+
+//          // Assert
+//          Assert.IsNotNull(result);
+//          Assert.AreEqual(2, result.Count());
+//          Assert.AreEqual("value1", result.ElementAt(0));
+//          Assert.AreEqual("value2", result.ElementAt(1));
+//      }
+
+//      [TestMethod]
+//      public void GetById()
+//      {
+//          // Arrange
+//          EmployeeManagementController controller = new EmployeeManagementController();
+
+//          // Act
+//          string result = controller.Get(5);
+
+//          // Assert
+//          Assert.AreEqual("value", result);
+//      }
+
+//      [TestMethod]
+//      public void Post()
+//      {
+//          // Arrange
+//          ValuesController controller = new ValuesController();
+
+//          // Act
+//          controller.Post("value");
+
+//          // Assert
+//      }
+
+//      [TestMethod]
+//      public void Put()
+//      {
+//          // Arrange
+//          ValuesController controller = new ValuesController();
+
+//          // Act
+//          controller.Put(5, "value");
+
+//          // Assert
+//      }
+
+//      [TestMethod]
+//      public void Delete()
+//      {
+//          // Arrange
+//          EmployeeManagementController controller = new EmployeeManagementController();
+
+//          // Act
+//          controller.Delete(5);
+
+//          // Assert
+//      }
+//  }
+//}
+
